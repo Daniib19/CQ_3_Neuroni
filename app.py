891c0fa157a644abed541fd42237affad155e255
Daniib19 <lungan.daniel2002@yahoo.com>
2025-12-05 12:47:58 +0200

    yolo + config files

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("test")
+import config as cfg
+from ultralytics import YOLO
+
+yolo_model = YOLO(cfg.YOLO_PATH)
+
+if __name__ == "__main__":
+  print("Model loaded from:", cfg.YOLO_PATH)
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_PATH    = os.path.dirname(os.path.abspath(__file__))
+MODELS_PATH  = os.path.join(BASE_PATH, "models")
+
+YOLO_PATH    = os.path.join(MODELS_PATH, "yolov8n.pt")
